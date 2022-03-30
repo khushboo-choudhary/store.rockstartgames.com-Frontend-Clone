e71a1c8925f633d38a5f2dea58e63d01aae6c09c
aman-638 <ar7009297@gmail.com>
2022-03-30 21:56:57 +0530

    collections grand component shark card

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import Grand from "./components/Grand.Collection";
+
 function App() {
   return (
     <div className='App'>
     <h1 className='ml-4 text-3xl font-bold underline animate-bounce hover:bg-red-500'>
       Hello world! Thanks for Using TailwindConfigure Package
     </h1>
+    <Grand/>
     </div>
   );
 }
