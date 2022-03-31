c0f69e69ac6eacef5453c1e1e63aabfab63edd3e
aman-638 <ar7009297@gmail.com>
2022-03-31 10:58:28 +0530

    grand style and added data

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,16 @@
-<<<<<<< HEAD:src/App.js
 import Navbar from './components/Navbar' 
 import Svg from './components/Svg' 
 import Footer from './components/Footer' 
-=======
-import Grand from "./components/Grand.Collection";
 import AllRoutes from './Route/Routes';
->>>>>>> 853b97e2a6ee443a70d3ae64863467038100f2c1:src/App.jsx
 
 function App() {
   return (
     <>
     <div className='App'>
-<<<<<<< HEAD:src/App.js
-
-  
     <Navbar/>
+    <AllRoutes/>
     <Svg/>
     <Footer/>
-=======
-    <h1 className='ml-4 text-3xl font-bold underline animate-bounce hover:bg-red-500'>
-      Hello world! Thanks for Using TailwindConfigure Package
-    </h1>
-    <AllRoutes/>
->>>>>>> 853b97e2a6ee443a70d3ae64863467038100f2c1:src/App.jsx
     </div>
     
     </>
