3e94e4a05d79bd2383ecc10a39a681fec25ab766
aman-638 <ar7009297@gmail.com>
2022-04-01 18:35:18 +0530

    product page some chages

--- src/components/Gear/Product/Product.jsx
+++ src/components/Gear/Product/Product.jsx
@@ -8,12 +8,13 @@ const Product = () => {
   const [product, setproduct] = useState([]);
   const [apparelData, setapparelData] = useState([]);
 
+
   useEffect(() => {
     axios
       .get(`https://obscure-citadel-15133.herokuapp.com/gear/${id}`)
       .then((res) => {
         setproduct(res.data);
-        console.log(res.data);
+        console.log("gear data",res.data);
       });
     axios
       .get("https://obscure-citadel-15133.herokuapp.com/gear/apparel")
@@ -24,88 +25,83 @@ const Product = () => {
 
   return (
     <>
-      <div id="header-part">
-        <p>Header</p>
-      </div>
+    <div id="header-part">
+    </div>
 
-      {product.map((curElem) => {
-        return (
-          <>
-            <div id="product-part">
-              <div id="product-left-part">
-                <div id="main-img-part">
-                  <img src={curElem.image} alt="" />
-                </div>
-                <div id="other-img-part">
-                  {curElem.allimages.map((el) => {
-                    <img src={el} alt="images" />;
-                  })}
-                </div>
+          <div id="product-part">
+            <div id="product-left-part">
+              <div id="main-img-part">
+                <img src={product.image} alt="" />
               </div>
-              <div id="product-right-part">
-                <div id="prod-head">
-                  <p>{curElem.name}</p>
-                </div>
-                <div id="prod-desc">
-                  <p>{curElem.description}</p>
-                </div>
-                <div id="prod-price">
-                  <p>€19.99</p>
-                </div>
-                <div id="user-gender">
-                  <p>Select Gender</p>
-                </div>
-                <div id="user-gender-btn">
-                  <div id="mens-btn">Men's</div>
-                  <div id="womens-btn">Women's</div>
-                </div>
-                <div id="user-size">
-                  <p>Select Size</p>
-                </div>
-                <div id="user-size-btn">
-                  <span>S</span>
-                  <span>M</span>
-                  <span>L</span>
-                  <span>XL</span>
-                  <span>XXL</span>
-                  <span>XXXL</span>
-                </div>
-                <div id="buy-add-btn">
-                  <div id="buy-btn">Buy Now</div>
-                  <div id="add-btn">Add to cart</div>
-                </div>
+              <div id="other-img-part">
+                {/* {product.allimages.map((el) => (
+                  <img src={el} alt="images" />
+                ))} */}
               </div>
             </div>
-          </>
-        );
-      })}
+            <div id="product-right-part">
+              <div id="prod-head">
+                <p>{product.name}</p>
+              </div>
+              <div id="prod-desc">
+                <p>{product.description}</p>
+              </div>
+              <div id="prod-price">
+                <p>€{product.price}</p>
+              </div>
+              <div id="user-gender">
+                <p>Select Gender</p>
+              </div>
+              <div id="user-gender-btn">
+                <div id="mens-btn">Men's</div>
+                <div id="womens-btn">Women's</div>
+              </div>
+              <div id="user-size">
+                <p>Select Size</p>
+              </div>
+              <div id="user-size-btn">
+                <span>S</span>
+                <span>M</span>
+                <span>L</span>
+                <span>XL</span>
+                <span>XXL</span>
+                <span>XXXL</span>
+              </div>
+              <div id="buy-add-btn">
+                <div id="buy-btn">Buy Now</div>
+                <div id="add-btn">Add to cart</div>
+              </div>
+            </div>
+          </div>
+       
+   
 
-      {/* Related Products */}
-      <div id="apparel-items-part">
-        <div id="apparel-heading-part">
-          <p class="apparel-heading-rohit">Realted Products</p>
-          <p>SEE ALL</p>
-        </div>
-        <div id="all-card-container">
-          {apparelData.map((curElem) => {
-            return (
-              <div>
-                <div id="apparel-items-content">
-                  <div id="apparel-item-card">
-                    <div id="apparel-item-img-part">
-                      <img src={curElem.image} alt="" />
-                    </div>
-                    <div id="apparel-item-title-part">
-                      <p>{curElem.name}</p>
+            {/* Related Products */}
+            <div id="apparel-items-part">
+            <div id="apparel-heading-part">
+              <p class="apparel-heading-rohit">Realted Products</p>
+              <p>SEE ALL</p>
+            </div>
+            <div id="all-card-container">
+              {apparelData.map((curElem) => {
+                return (
+                  <div>
+                    <div id="apparel-items-content">
+                      <div id="apparel-item-card">
+                        <div id="apparel-item-img-part">
+                          <img src={curElem.image} alt="" />
+                        </div>
+                        <div id="apparel-item-title-part">
+                          <p>{curElem.name}</p>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-    </>
+                );
+              })}
+            </div>
+          </div>
+        </>
   );
 };
 
