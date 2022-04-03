39b0edb0556931797d583136adb2cff2c760ed23
Shreyasgkhakal100 <shreyasgkhakal100@gmail.com>
2022-04-03 13:00:24 +0530

    social login done

--- src/components/gooogleCallBack.jsx
+++ src/components/gooogleCallBack.jsx
@@ -1,10 +1,24 @@
+import { useLocation } from "react-router-dom";
 
+export const GoogleCallBack = () => {
 
-export const GoogleCallBack = ()=>{
+      const search = useLocation().search;
+      const token = new URLSearchParams(search).get("token");
+      const nickName = new URLSearchParams(search).get("nickName");
+      const profileImage = new URLSearchParams(search).get("profileImage");
 
-    return (
-        <div>
-            <h1>Google CallBack</h1>
-        </div>
-    )
-}
+          localStorage.setItem("token", JSON.stringify(token));
+          localStorage.setItem(
+            "userData",
+            JSON.stringify({ profileImage, nickName })
+          );
+          alert("You Have LoggedIn Successfully")
+            // navigate("/");
+            window.location.href = "/";
+
+  return (
+    <div>
+      <h1>Google CallBack</h1>
+    </div>
+  );
+};
